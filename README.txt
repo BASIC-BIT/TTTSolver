7456cb36c5483edd65995d034d18beb26ff68f81
Steven Kneisler <stevenkneisler@gmail.com>
2012-08-19 15:35:30 -0400

    distboard function added and readme update

--- README.txt
+++ README.txt
@@ -1,1 +1,21 @@
-This is a placeholder for the Tic Tac Toe solver README (test)
+Tic Tac Toe Solver
+by Stevenator1 (Steven Kneisler)
+Verson 0.0.1
+
+This program is a test of simple decision making skills.  No winning conditions are hardcoded into the game.  Only whether it has won or lost.
+
+Commands:
+human - Have the computer play against you.
+comp - Have the computer play one game against itself.
+comp # - Have the computer play # games against itself.
+
+When playing against the computer, you type the square value to enter your O.
+
+The square values are:
+[1,2,3]
+[4,4,6]
+[7,8,9]
+
+Currently in human games, the computer will always play X.  That will be changed in the future.
+
+This game, code, and all related materials is private and is not to be used without express permission from the author.
---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+void distboard(int value)
+{
+    int valleft=value;
+    string vals[9];
+    for(int n=8;n>=0;n--)
+    {
+        bool found = false;
+        for(int nn=2;nn>0;nn--)
+        {
+            if(valleft >=nn*pow(3,n))
+            {
+                if(nn==1)
+                {
+                    vals[n]="X";
+                }
+                if(nn==2)
+                {
+                    vals[n]="O";
+                }
+                found=true;
+                valleft = valleft - nn*pow(3,n);
+            }
+        }
+        if(found==false)
+        {
+            vals[n]=" ";
+        }
+    }
+    cout << "[" << vals[0] << " " << vals[1] << " " << vals[2] << "]" << endl;
+    cout << "[" << vals[3] << " " << vals[4] << " " << vals[5] << "]" << endl;
+    cout << "[" << vals[6] << " " << vals[7] << " " << vals[8] << "]" << endl;
+    return;
+}
+
+int main()
+{
+    distboard(11955);
+    return 0;
+}
